da5df28ded4d23b4b7d9a3e7ffb544129bc9d980
Ragnorok303 <jasontompkins22@gmail.com>
2020-04-30 10:29:21 -0600

    Created Database for page

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured ) VALUES ("Big Mac", False
+ );
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper", False
+);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie", False
+);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Krabby Patty", False
+);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Baconator", False
+);
