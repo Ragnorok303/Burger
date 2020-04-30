a40342792b36645dbc7f792001619f40d5e7e492
Ragnorok303 <jasontompkins22@gmail.com>
2020-04-30 11:28:09 -0600

    Added script to file

---
+++ models/burger.js
@@ -0,0 +1,25 @@
+var orm = require("../config/orm");
+
+var burger = {
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+
+  insertOne: function (name, cb) {
+    orm.insertOne("burgers", name, cb)
+  },
+
+  updateOne: function (id, cb) {
+    orm.updateOne("burgers", id, cb);
+  },
+
+  deleteOne: function (id, cb, name) {
+    orm.deleteOne("burgers", id, cb, name, function (res) {
+      cb(res)
+    });
+  }
+};
+
+module.exports = burger
